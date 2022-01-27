2846a21bd5f079591dcce21db6f4e19531bdec1b
cioko <brzepat@gmail.com>
2022-01-27 23:19:36 +0100

    Application of hints

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Interpreter interpreter = new Interpreter();
-        System.out.println(interpreter.interpret("2+2-6/2"));
+        System.out.println(interpreter.interpret("-2+2-6/2-100"));
 
 
     }
